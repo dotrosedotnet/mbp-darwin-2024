ee818b4d5d995228147ba6185d086f7bbadcb57a
Dot Rose <stuff@dottie.me>
2024-12-17 08:38:00 -0500

    lazygit

--- home.nix
+++ home.nix
@@ -6,6 +6,10 @@
   home.username = "dot";
   home.homeDirectory = /Users/dot;
 
+  home.packages = with pkgs; [
+    lazygit
+  ];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
